4a4e31a54cb32f0541ee61243b60eaf461899cd3
Smitgajjar <smitgajjar.gs@gmail.com>
2019-09-07 12:03:20 +0530

    init commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN = '952842089:AAFArpTFuaSuLCUB-fu1oKzAoDnUHN3sxjQ'
-NGROK_URL = 'https://797466da.ngrok.io'
+TOKEN = ''
+NGROK_URL = ''
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
 TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
